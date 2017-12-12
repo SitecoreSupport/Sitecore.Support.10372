7ff83c6ed88895b1a52d591097e7c049c24a38c0
DK\IRO <iro@sitecore.net>
2017-12-12 12:59:56 +0200

    Composites do not work on mobile devices

---
+++ src/Sitecore.Support.10372/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.10372")]
+[assembly: AssemblyProduct("Sitecore.Support.10372")]
+[assembly: ComVisible(false)]
